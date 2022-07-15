51b242d76b2c53cd85796280cd37a5644a3cef1f
Vedant478 <55829535+Vedant478@users.noreply.github.com>
2022-07-15 10:01:42 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,85 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  ImageBackground,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+
+} from 'react-native/Libraries/NewAppScreen';
+import CarItem from './components/CarItem';
+import CarList from './components/CarItem/CarList';
+import Headermenu from './components/CarItem/Headermenu'
+ 
+
+
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <View style={styles.container}>
+      
+      <Headermenu />
+      <CarList/>
+
+      
+
+      
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+ 
+  container:{
+    flex: 1,
+    backgroundColor:'#fff',
+    alignItems: 'center',
+    justifyContent:'center'
+
+  },
+
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
