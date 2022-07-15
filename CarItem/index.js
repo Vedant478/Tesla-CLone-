7d0ddae88ac83c27545bc1a4d1b171eb1e0e83a5
Vedant478 <55829535+Vedant478@users.noreply.github.com>
2022-07-15 10:03:55 +0530

    Create index.js

---
+++ CarItem/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { View,Text,ImageBackground } from 'react-native';
+import styles from './styles';
+import StyledButton from './StyledButton';
+const CarItem =(props) => {
+
+  const{name,tagline,taglineCTA,image}= props.car;
+
+    return(
+        <View style={styles.carContainer}>
+        <ImageBackground source={image}
+        style={styles.image}
+        
+        />
+
+        <View style={styles.titles}>
+          <Text style={styles.title}>{name}</Text>
+          <Text style={styles.subtitle}>{tagline}
+          {' '}
+          <Text style={{
+            textDecorationLine: 'underline',
+          }}>
+            {taglineCTA}
+          </Text>
+          
+          </Text>
+
+        </View>
+        <View style={{
+          position: 'absolute',
+          bottom: 50,
+          width: '100%',
+        }}>
+        <StyledButton 
+        type="primary"
+        content={"Custom Order"}
+        onPress={() =>{
+          console.warn("Custom Order was Pressed");
+        }}
+        />
+        <StyledButton 
+        type="secondary"
+        content={"Existing Inventory"}
+        onPress={() =>{
+          console.warn("Existing Inventory was Pressed");
+        }}
+        />
+        </View>
+       
+      </View>
+
+    );
+};
+export default CarItem;
